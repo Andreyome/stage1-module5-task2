0d9184f0a5fe0e4fb5ab36f87ec209829f1937fa
Andreyome <andreyomem@gmail.com>
2023-03-08 20:53:59 +0200

    Revert "commit 1"
    This reverts commit dc8241d4087e30dad3fa212be9ffb7caf1ce2d5c.

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,16 +1,8 @@
 package com.epam.mjc;
 
-import java.util.LinkedList;
-
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-return input ->{
-    LinkedList<Integer> out = new LinkedList<>();
-    for (int tmp : input) {
-        out.add(tmp/divider);
-    }
-    return out;
-};
+        throw new UnsupportedOperationException("You should implement this method.");
     }
 }
